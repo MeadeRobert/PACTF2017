46c96e04491007d3299a089fe0d8e2fe3c5501c3
MeadeRobert <meade.develop3r@gmail.com>
2017-05-16 22:37:19 -0400

    update tiebreaker soln

--- PACTF2017_Tiebreaker/problem/solution.py
+++ PACTF2017_Tiebreaker/problem/solution.py
@@ -4,6 +4,7 @@ from incredible_hash import *
 import itertools as it
 
 # 126 is ord('~')
+# not valid if hash_number < 32
 def end_string(hash_number):
         passwd = ""
         passwd_number = 0
@@ -12,11 +13,14 @@ def end_string(hash_number):
         while passwd_number < hash_number - 126:
                 passwd += '~'
                 passwd_number += 126
+                # return endstring from existing table if possible
+                if hash_number - passwd_number in end_strings:
+                        return passwd + end_strings[hash_number - passwd_number]
 
         # handle special cases for last value
-        last_value = hash_number - passwd_number
+        last_value = hash_number - passwd_number        
         # if last_value < 32 use 2 separate chars to make value
-        if last_value < 32 and len(passwd) > 0:
+        if last_value < 32:
                 passwd = passwd[0:len(passwd)-1]
                 last_value += 126
                 if last_value % 2 == 0:
@@ -24,7 +28,7 @@ def end_string(hash_number):
                 else:
                         passwd += chr(last_value // 2) + chr(last_value // 2 + 1)
         # if last_value on [60, 62] use chars in valid range to make value
-        elif last_value in range(60, 63) and len(passwd) > 0:
+        elif last_value in xrange(60, 63):
                 passwd = passwd[0:len(passwd)-1]
                 last_value += 126
                 passwd += chr(120) + chr(last_value - 120)
@@ -36,13 +40,13 @@ def end_string(hash_number):
 
 # generate map of ending strings
 end_strings = {}
-for i in it.chain(range(32, 60), range(63, 4032)):
+for i in it.chain(xrange(32, 60), xrange(63, 4032)):
         end_strings[i] = end_string(i)
 #print "end_strings =",end_strings
         
 # generate map of hashes
 hashes = {}
-for i in range(32, 4032):
+for i in it.chain(xrange(32, 60), xrange(63, 4032)):
         hash = hash_number(i)
         if hash in hashes:
                 hashes[hash].append(i)
@@ -54,5 +58,5 @@ for i in range(32, 4032):
 def solution(cutoff_string, final_hash):
         passwd_number = sum(bytearray(cutoff_string))
         for hash_number in hashes[final_hash]:
-               if hash_number - passwd_number in end_strings.keys():
-                       return cutoff_string + end_strings[hash_number - passwd_number]
+                if hash_number - passwd_number in end_strings.keys():
+                        return cutoff_string + end_strings[hash_number - passwd_number]
